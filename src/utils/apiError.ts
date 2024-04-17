68ce083c0e630d2118765e59ed6e77b1488b2b04
bp26 <aleshak260006@gmail.com>
2024-04-17 16:08:26 +0300

    feat: add ApiError

---
+++ src/utils/apiError.ts
@@ -0,0 +1,11 @@
+import { StatusCode } from '../enums';
+
+class ApiError extends Error {
+  status: number;
+  constructor(status: StatusCode, message: string) {
+    super(message);
+    this.status = +status;
+  }
+}
+
+export default ApiError;
