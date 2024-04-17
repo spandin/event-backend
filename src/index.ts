d8f78ad33de02f9cb656c45fedbbcb101a9a662c
bp26 <aleshak260006@gmail.com>
2024-04-17 16:10:16 +0300

    feat: add initialize func to index.ts

---
+++ src/index.ts
@@ -0,0 +1,35 @@
+import express from 'express';
+import passport from 'passport';
+import routes from './routers';
+import session from 'express-session';
+import { PORT, SESSION_MAX_AGE, SESSION_SECRET } from './constants';
+
+const SERVER_PORT = process.env.PORT || PORT;
+
+const initialize = () => {
+  const app = express();
+
+  app.use(express.json());
+
+  app.use(
+    session({
+      secret: process.env.SESSION_SECRET || SESSION_SECRET,
+      saveUninitialized: false,
+      resave: false,
+      cookie: {
+        maxAge: SESSION_MAX_AGE,
+      },
+    })
+  );
+
+  app.use(passport.initialize());
+  app.use(passport.session());
+
+  app.use(routes);
+
+  app.listen(SERVER_PORT, () => {
+    console.log(`Server is running on port ${SERVER_PORT}`);
+  });
+};
+
+initialize();
