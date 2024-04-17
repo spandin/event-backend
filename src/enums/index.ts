73e9252acbc989e829b020d5bdc33e9df5537e72
bp26 <aleshak260006@gmail.com>
2024-04-17 15:59:25 +0300

    feat: add enum StatusCode

---
+++ src/enums/index.ts
@@ -0,0 +1,9 @@
+export const enum StatusCode {
+  OK = '200',
+  BAD_REQUEST = '400',
+  UNAUTHORIZED = '401',
+  FORBIDDEN = '403',
+  NOT_FOUND = '404',
+  CONFLICT = '409',
+  SERVER_ERROR = '500',
+}
