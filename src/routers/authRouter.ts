553e8ff38354c1cc7329dc643115a56f0d6f2a10
bp26 <aleshak260006@gmail.com>
2024-04-20 22:12:21 +0300

    feat: add /google and /google/callback routes to authRouter

--- src/routers/authRouter.ts
+++ src/routers/authRouter.ts
@@ -6,8 +6,25 @@ import { isUserAuthenticated } from '../middleware/isUserAuthenticated';
 const authRouter = express.Router();
 
 authRouter.post('/register', authController.register);
-authRouter.post('/login', passport.authenticate('local'), authController.login);
-authRouter.get('/user', isUserAuthenticated, authController.login);
+authRouter.post(
+  '/login',
+  passport.authenticate('local'),
+  authController.getAuthentificatedUser
+);
+authRouter.get(
+  '/google',
+  passport.authenticate('google', { scope: ['profile', 'email'] })
+);
+authRouter.get(
+  '/google/callback',
+  passport.authenticate('google', { successRedirect: '/' }),
+  authController.getAuthentificatedUser
+);
+authRouter.get(
+  '/user',
+  isUserAuthenticated,
+  authController.getAuthentificatedUser
+);
 authRouter.delete('/logout', isUserAuthenticated, authController.logout);
 
 export default authRouter;
