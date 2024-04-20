f0c472779e2906c1dc575e0f8a16b53d701f5e25
bp26 <aleshak260006@gmail.com>
2024-04-20 21:54:31 +0300

    feat: update UserRepository class

--- src/repositories/userRepository.ts
+++ src/repositories/userRepository.ts
@@ -1,28 +1,59 @@
 import prisma from '../prisma';
+import { user } from '@prisma/client';
 
 class UserRepository {
-  async getUserByEmail(email: string) {
+  async getByEmail(email: string) {
     return await prisma.user.findFirst({
       where: {
         email,
       },
+      include: {
+        google_user: true,
+        local_user: true,
+      },
     });
   }
 
-  async getUserById(id: number) {
+  async getById(id: string) {
     return await prisma.user.findFirst({
       where: {
         id,
       },
+      include: {
+        google_user: true,
+        local_user: true,
+      },
     });
   }
 
-  async createUser(email: string, password: string) {
+  async getByGoogleId(google_id: string) {
+    return await prisma.user.findFirst({
+      where: {
+        google_user: {
+          google_id,
+        },
+      },
+      include: {
+        google_user: true,
+        local_user: true,
+      },
+    });
+  }
+
+  async create(email: string) {
     return await prisma.user.create({
       data: {
         email,
-        password,
       },
+    });
+  }
+
+  async update(id: string, user: Partial<user>) {
+    return await prisma.user.update({
+      where: {
+        id,
+      },
+      data: { ...user },
     });
   }
 }
