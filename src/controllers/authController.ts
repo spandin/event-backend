c7a29798f55095ed074914b1666cb5b1ff41dbda
bp26 <aleshak260006@gmail.com>
2024-04-21 19:57:34 +0300

    feat: add try/catch blocks for methods in AuthController class

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,4 +1,4 @@
-import { Request, Response } from 'express'
+import { NextFunction, Request, Response } from 'express'
 import ApiError from '../utils/apiError'
 import { ResponceMessage, StatusCode } from '../enums'
 import authService from '../services/authService'
@@ -6,25 +6,37 @@ import { registrationSchema } from '../schemas'
 import isDataValid from '../utils/isDataValid'
 
 class AuthController {
-  async registerLocalUser(req: Request, res: Response) {
-    const { email, password } = req.body
-    if (!isDataValid(registrationSchema, { email, password })) {
-      throw new ApiError(StatusCode.BAD_REQUEST, ResponceMessage.WRONG_PROPS)
+  async registerLocalUser(req: Request, res: Response, next: NextFunction) {
+    try {
+      const { email, password } = req.body
+      if (!isDataValid(registrationSchema, { email, password })) {
+        throw new ApiError(StatusCode.BAD_REQUEST, ResponceMessage.WRONG_PROPS)
+      }
+
+      const user = await authService.registerLocalUser(email, password)
+
+      res.status(+StatusCode.OK).send(user)
+    } catch (error) {
+      next(error)
     }
-
-    const user = await authService.registerLocalUser(email, password)
-
-    res.status(+StatusCode.OK).send(user)
   }
 
-  logout(req: Request, res: Response) {
-    req.logout(() => {
-      res.status(+StatusCode.OK).send(ResponceMessage.LOGOUT)
-    })
+  logout(req: Request, res: Response, next: NextFunction) {
+    try {
+      req.logout(() => {
+        res.status(+StatusCode.OK).send(ResponceMessage.LOGOUT)
+      })
+    } catch (error) {
+      next(error)
+    }
   }
 
-  getAuthentificatedUser(req: Request, res: Response) {
-    res.status(+StatusCode.OK).send({ user: req.user, message: ResponceMessage.LOGIN })
+  getAuthentificatedUser(req: Request, res: Response, next: NextFunction) {
+    try {
+      res.status(+StatusCode.OK).send({ user: req.user, message: ResponceMessage.LOGIN })
+    } catch (error) {
+      next(error)
+    }
   }
 }
 
