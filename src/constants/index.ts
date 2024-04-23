2034d5a802c03727c6d51b8228dbe636bd1a5467
bp26 <aleshak260006@gmail.com>
2024-04-23 22:46:26 +0300

    fix: change CORS_CONFIG

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -2,8 +2,6 @@ export const PORT = 3000
 export const SESSION_MAX_AGE = 2592000000
 export const SALT_ROUNDS = 10
 export const CORS_CONFIG = {
-  origin: ['http://localhost:5173'],
-  optionsSuccessStatus: 200,
-  credentials: true,
-  preflightContinue: true
+  origin: 'http://localhost:5173',
+  credentials: true
 }
