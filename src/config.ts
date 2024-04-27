c57a325bc03c177e9057bd24d384950edcf2d0a3
bp26 <aleshak260006@gmail.com>
2024-04-27 22:16:00 +0300

    feat: update SESSION_CONFIG

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,8 @@
 import { CorsOptions } from 'cors'
 import { SessionOptions } from 'express-session'
+import { PrismaSessionStore } from '@quixo3/prisma-session-store'
 import { SESSION_MAX_AGE, CLIENT_BASE_URL } from './constants/index.js'
+import prisma from './prisma.js'
 
 export const SESSION_CONFIG: SessionOptions = {
   secret: process.env.SESSION_SECRET || '',
@@ -8,7 +10,11 @@ export const SESSION_CONFIG: SessionOptions = {
   resave: false,
   cookie: {
     maxAge: SESSION_MAX_AGE
-  }
+  },
+  store: new PrismaSessionStore(prisma, {
+    dbRecordIdIsSessionId: true,
+    dbRecordIdFunction: undefined
+  })
 }
 
 export const CORS_CONFIG: CorsOptions = {
