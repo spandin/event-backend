e5b000cff58a91a076c45d7ed67731b5d2a96061
Aliaksey <aleshak260006@gmail.com>
2024-05-02 15:25:26 +0300

    feat: add createEventSchema

--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -9,3 +9,10 @@ export const loginSchema = z.object({
   email: z.string().email(),
   password: z.string()
 })
+
+export const createEventSchema = z.object({
+  title: z.string(),
+  description: z.string().optional(),
+  location: z.string().optional(),
+  date: z.date()
+})
