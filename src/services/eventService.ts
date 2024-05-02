7b963010af173b6907e928d1271db585a5294b65
Aliaksey <aleshak260006@gmail.com>
2024-05-02 15:59:12 +0300

    feat: add method isUserAMember to EventService class

--- src/services/eventService.ts
+++ src/services/eventService.ts
@@ -1,3 +1,25 @@
-class EventService {}
+import { ResponceMessage, StatusCode } from '../enums/index.js'
+import prisma from '../prisma.js'
+import eventRepository from '../repositories/eventRepository.js'
+import userRepository from '../repositories/userRepository.js'
+import ApiError from '../utils/apiError.js'
+
+class EventService {
+  private async isUserAMember(event_id: string, user_id: string) {
+    const [event, user] = await prisma.$transaction([eventRepository.getOneByEventId(event_id), userRepository.getOneById(user_id)])
+
+    if (!user) {
+      throw new ApiError(StatusCode.NOT_FOUND, ResponceMessage.USER_DOESNT_EXIST)
+    }
+
+    if (!event) {
+      throw new ApiError(StatusCode.NOT_FOUND, ResponceMessage.EVENT_DOESNT_EXIST)
+    }
+
+    const isUserAMember = event.members.find((member) => member.user_id === user_id)
+
+    return isUserAMember
+  }
+}
 
 export default new EventService()
