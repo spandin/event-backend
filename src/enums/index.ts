f86c608b36eef94c205f08774a8ad800f702ca12
Aliaksey <aleshak260006@gmail.com>
2024-05-02 15:18:27 +0300

    feat: update ResponseMessage enum

--- src/enums/index.ts
+++ src/enums/index.ts
@@ -9,12 +9,21 @@ export const enum StatusCode {
 }
 
 export const enum ResponceMessage {
-  USER_ALREADY_EXISTS = 'This email is already in use',
-  USER_DOESNT_EXIST = 'This user is not registered',
-  USER_WRONG_CREDENTIALS = 'The passed credentials are incorrect',
   WRONG_PROPS = 'Required properties are either not passed or have wrong format',
   UNAUTHORIZED = 'User not authorized',
+
   LOGIN = 'You have successfully logged in',
   REGISTER = 'You have successfully registered',
-  LOGOUT = 'You have successfully logged out'
+  LOGOUT = 'You have successfully logged out',
+
+  USER_ALREADY_EXISTS = 'This email is already in use',
+  USER_DOESNT_EXIST = `This user doesn't exist`,
+  USER_WRONG_CREDENTIALS = 'The passed credentials are incorrect',
+
+  EVENT_CREATED = 'Event successfully created',
+  EVENT_DELETED = 'Event successfully deleted',
+  EVENT_UPDATED = 'Event successfully updated',
+  EVENT_DOESNT_EXIST = `Event doesn't exist`,
+  EVENT_USER_ALREADY_MEMBER = 'User is already a member',
+  EVENT_USER_NOT_MEMBER = 'User is not a member'
 }
