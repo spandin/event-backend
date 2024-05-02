1f9789eeaf1666f4e464fa93af98954fd4aa6a4a
Aliaksey <aleshak260006@gmail.com>
2024-05-02 16:42:49 +0300

    feat: add route in eventRouter for createEvent

--- src/routers/eventRouter.ts
+++ src/routers/eventRouter.ts
@@ -1,8 +1,10 @@
 import express from 'express'
 import { isUserAuthenticated } from '../middleware/isUserAuthenticated.js'
+import eventController from '../controllers/eventController.js'
 
 const eventRouter = express.Router()
 
 eventRouter.use(isUserAuthenticated)
+eventRouter.post('', eventController.createEvent)
 
 export default eventRouter
