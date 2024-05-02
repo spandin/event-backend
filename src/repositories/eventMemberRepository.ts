cc912d3c872821b86cbf5e45b1880e0715c071e9
Aliaksey <aleshak260006@gmail.com>
2024-05-02 15:44:11 +0300

    feat: add class EventMemberRepository

---
+++ src/repositories/eventMemberRepository.ts
@@ -0,0 +1,72 @@
+import { event_member } from '@prisma/client'
+import prisma from '../prisma.js'
+
+class EventMemberRepository {
+  getAllByUserId(user_id: string) {
+    return prisma.event_member.findMany({
+      where: {
+        user_id
+      },
+      include: {
+        event: true
+      }
+    })
+  }
+
+  getAllByEventId(event_id: string) {
+    return prisma.event_member.findMany({
+      where: {
+        event_id
+      },
+      include: {
+        event: true
+      }
+    })
+  }
+
+  createOne(user_id: string, event_id: string) {
+    return prisma.event_member.create({
+      data: {
+        user_id,
+        event_id
+      }
+    })
+  }
+
+  updateOneByEventIdAndUserId(event_id: string, user_id: string, data: Partial<event_member>) {
+    return prisma.event_member.update({
+      where: {
+        event_id,
+        user_id
+      },
+      data
+    })
+  }
+
+  deleteAllByEventId(event_id: string) {
+    return prisma.event_member.deleteMany({
+      where: {
+        event_id
+      }
+    })
+  }
+
+  deleteAllByUserId(user_id: string) {
+    return prisma.event_member.deleteMany({
+      where: {
+        user_id
+      }
+    })
+  }
+
+  deleteOneByEventIdAndUserId(event_id: string, user_id: string) {
+    return prisma.event_member.delete({
+      where: {
+        event_id,
+        user_id
+      }
+    })
+  }
+}
+
+export default new EventMemberRepository()
