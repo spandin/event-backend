dfabae10dadecb9a3618e4dd73ca879250a97bdd
Aliaksey <aleshak260006@gmail.com>
2024-05-03 00:13:13 +0300

    fix: fix eventRouter

--- src/routers/eventRouter.ts
+++ src/routers/eventRouter.ts
@@ -6,10 +6,10 @@ const eventRouter = express.Router()
 
 eventRouter.use(isUserAuthenticated)
 eventRouter.get('', eventController.getEventsByUserId)
-eventRouter.get(':id', eventController.getEventByEventId)
+eventRouter.get('/:id', eventController.getEventByEventId)
 eventRouter.post('', eventController.createEvent)
-eventRouter.delete(':id', eventController.deleteEvent)
-eventRouter.patch(':id', eventController.updateEvent)
+eventRouter.delete('/:id', eventController.deleteEvent)
+eventRouter.patch('/:id', eventController.updateEvent)
 eventRouter.patch('/member/:id', eventController.addMemberToEvent)
 eventRouter.delete('/member/:id', eventController.deleteMemberFromEvent)
 
