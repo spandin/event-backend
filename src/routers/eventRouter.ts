6d80b7ecca8b0c2dba4c0434be71f46e66f2d099
Aliaksey <aleshak260006@gmail.com>
2024-05-02 17:34:13 +0300

    feat: update eventRouter

--- src/routers/eventRouter.ts
+++ src/routers/eventRouter.ts
@@ -8,5 +8,9 @@ eventRouter.use(isUserAuthenticated)
 eventRouter.get('', eventController.getEventsByUserId)
 eventRouter.get(':id', eventController.getEventByEventId)
 eventRouter.post('', eventController.createEvent)
+eventRouter.delete(':id', eventController.deleteEvent)
+eventRouter.patch(':id', eventController.updateEvent)
+eventRouter.patch('/member/:id', eventController.addMemberToEvent)
+eventRouter.delete('/member/:id', eventController.deleteMemberFromEvent)
 
 export default eventRouter
