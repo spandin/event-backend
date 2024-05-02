353fda4c2c361c0b6a0cc54f16dd0fdb2949d5ac
Aliaksey <aleshak260006@gmail.com>
2024-05-02 16:00:26 +0300

    feat: add method addMemberToEvent in EventService class

--- src/services/eventService.ts
+++ src/services/eventService.ts
@@ -1,10 +1,23 @@
 import { ResponceMessage, StatusCode } from '../enums/index.js'
 import prisma from '../prisma.js'
+import eventMemberRepository from '../repositories/eventMemberRepository.js'
 import eventRepository from '../repositories/eventRepository.js'
 import userRepository from '../repositories/userRepository.js'
 import ApiError from '../utils/apiError.js'
 
 class EventService {
+  async addMemberToEvent(event_id: string, user_id: string) {
+    const isUserAMember = await this.isUserAMember(event_id, user_id)
+
+    if (isUserAMember) {
+      throw new ApiError(StatusCode.CONFLICT, ResponceMessage.EVENT_USER_ALREADY_MEMBER)
+    }
+
+    const eventMember = await eventMemberRepository.createOne(user_id, event_id)
+
+    return eventMember
+  }
+
   private async isUserAMember(event_id: string, user_id: string) {
     const [event, user] = await prisma.$transaction([eventRepository.getOneByEventId(event_id), userRepository.getOneById(user_id)])
 
