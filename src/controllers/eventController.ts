9cee5394a198cf102533c95bd3277b272c63f75d
Aliaksey <aleshak260006@gmail.com>
2024-05-02 16:38:41 +0300

    feat: add method createEvent in EventController class

--- src/controllers/eventController.ts
+++ src/controllers/eventController.ts
@@ -1,3 +1,27 @@
-class EventController {}
+import { NextFunction, Request, Response } from 'express'
+import { StatusCode, ResponceMessage } from '../enums/index.js'
+import ApiError from '../utils/apiError.js'
+import { createEventSchema } from '../schemas/index.js'
+import eventService from '../services/eventService.js'
+
+class EventController {
+  async createEvent(req: Request, res: Response, next: NextFunction) {
+    try {
+      const { title, description, location } = req.body
+
+      const validation = createEventSchema.safeParse({ title, description, location })
+
+      if (!validation.success) {
+        throw new ApiError(StatusCode.BAD_REQUEST, ResponceMessage.WRONG_PROPS)
+      }
+
+      const event = await eventService.createEvent(req.user!.id, validation.data)
+
+      res.status(+StatusCode.OK).send({ data: { event }, message: ResponceMessage.EVENT_CREATED })
+    } catch (error) {
+      next(error)
+    }
+  }
+}
 
 export default new EventController()
