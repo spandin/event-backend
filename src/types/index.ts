eab582c9870c3fe0c14415f6d5bfb4f4d4efbe41
Aliaksey <aleshak260006@gmail.com>
2024-05-04 18:17:00 +0300

    refactor: move schema types to a separate types file

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,8 +1,1 @@
-import z from 'zod'
-import { createEventSchema, updateEventSchema } from '../schemas/index.js'
-import { PrismaClient } from '@prisma/client'
-
-export type CreateEvent = z.infer<typeof createEventSchema>
-export type UpdateEvent = z.infer<typeof updateEventSchema>
-
-export type PrismaTransactionClient = Omit<PrismaClient, '$connect' | '$disconnect' | '$on' | '$transaction' | '$use' | '$extends'>
+export { UpdateEvent, CreateEvent } from './schemas.js'
---
+++ src/types/schemas.ts
@@ -0,0 +1,5 @@
+import { z } from 'zod'
+import { createEventSchema, updateEventSchema } from '../schemas/index.js'
+
+export type CreateEvent = z.infer<typeof createEventSchema>
+export type UpdateEvent = z.infer<typeof updateEventSchema>
